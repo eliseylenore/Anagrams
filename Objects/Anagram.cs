20c0786a16aec862d22e30b557c6c83b893dc5b7
Elise St Hilaire and Minh Phuong <elise+minh@>
2017-02-14 14:23:06 -0800

    add method to pass partial match

--- Objects/Anagram.cs
+++ Objects/Anagram.cs
@@ -21,12 +21,13 @@ namespace Anagrams.Objects
     {
       return _input;
     }
+
     public void SetInput( string MyInput)
     {
       _input = MyInput;
     }
 
-    public string SortString()
+    public List<string> SortString()
     {
       char[] inputArray = _input.ToCharArray();
       foreach (char letter in inputArray)
@@ -34,31 +35,51 @@ namespace Anagrams.Objects
         _inputList.Add(letter);
       };
       var result = _inputList.Select(c => c.ToString()).ToList();
-      result.Sort();
-      _output = string.Join( "", result.ToArray() );
-      return _output;
+      return result;
     }
 
-    public static bool Check(string userString, string _output)
-    {
-      if (userString == _output)
-      {return true;}
-      else
-      {return false;}
-    }
 
-    public List<string> SortSentence(string sentence, string _output) {
+    public List<string> SortSentence(string sentence, List<string> myList) {
       string[] splitSentence = sentence.Split(' ');
       foreach (string word in splitSentence)
       {
         Anagram phrases = new Anagram(word);
-        _userPhrase = phrases.SortString();
-        if (_userPhrase == _output)
+        List<string> result = phrases.SortString();
+        if (result.Intersect(myList).Count() == result.Count())
+        // result.Intersect(myList).Count() == result.Count()
         {
           _outputList.Add(word);
         }
       }
       return _outputList;
     }
+
+
+    // public string SortString()
+    // {
+    //   char[] inputArray = _input.ToCharArray();
+    //   foreach (char letter in inputArray)
+    //   {
+    //     _inputList.Add(letter);
+    //   };
+    //   var result = _inputList.Select(c => c.ToString()).ToList();
+    //   result.Sort();
+    //   _output = string.Join( "", result.ToArray() );
+    //   return _output;
+    // }
+    //
+    // public List<string> SortSentence(string sentence, string _output) {
+    //   string[] splitSentence = sentence.Split(' ');
+    //   foreach (string word in splitSentence)
+    //   {
+    //     Anagram phrases = new Anagram(word);
+    //     _userPhrase = phrases.SortString();
+    //     if (_output.Contains(_userPhrase))
+    //     {
+    //       _outputList.Add(word);
+    //     }
+    //   }
+    //   return _outputList;
+    // }
   }
 }
--- Tests/AnagramTest.cs
+++ Tests/AnagramTest.cs
@@ -7,23 +7,42 @@ namespace Anagrams.Objects
 {
   public class AnagramTest
   {
+    // [Fact]
+    // public void RandomizeWords_userInputandOurInput_true()
+    // {
+    //   //Arrange
+    //   string input = "Path";
+    //   Anagram testAnagram = new Anagram(input);
+    //   string outputString = testAnagram.SortString();
+    //
+    //   List<string> expectedOutput = new List<string>{"ahPt"};
+    //
+    //   //Act
+    //   string otherInputField = "Hello my name is Elise olleH lloHe ole ello llo ahPt hpt hat";
+    //   List<string> outputList = testAnagram.SortSentence(otherInputField, outputString);
+    //   foreach (string whatever in outputList)
+    //   {Console.WriteLine(whatever);};
+    //   //Assert
+    //   Assert.Equal(expectedOutput, outputList);
+    // }
+
     [Fact]
     public void RandomizeWords_userInputandOurInput_true()
     {
       //Arrange
       string input = "Hello";
       Anagram testAnagram = new Anagram(input);
-      string outputString = testAnagram.SortString();
+      List<string> outputList = testAnagram.SortString();
 
-      List<string> expectedOutput = new List<string>{"Hello"};
+      List<string> expectedOutput = new List<string>{"ahPt"};
 
       //Act
-      string otherInputField = "Hello my name is Elise olleH lloHe";
-      List<string> outputList = testAnagram.SortSentence(otherInputField, outputString);
-      foreach (string whatever in outputList)
+      string otherInputField = "Helo my name is Elise olleH lloHe ole ello llo ahPt hpt hat";
+      List<string> result = testAnagram.SortSentence(otherInputField, outputList);
+      foreach (string whatever in result)
       {Console.WriteLine(whatever);};
       //Assert
-      Assert.Equal(expectedOutput, outputList);
+      Assert.Equal(expectedOutput, result);
     }
   }
 }
